057e8b3bfeae3924c33c6985937f62fa519f1b68
Jean-Baptiste Bianchi <jb.bianchi@neuroglia.io>
2022-03-03 17:19:46 +0100

    Dashboard - added animated splash

---
+++ src/dashboard/Synapse.Dashboard/wwwroot/js/splash.js
@@ -0,0 +1,10 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const $animationContainer = document.getElementById('animation-container');
+    const removeContainer = (e) => {
+        if ($animationContainer.isSameNode(e.target)) {
+            $animationContainer.removeEventListener('animationend', removeContainer);
+            $animationContainer.remove();
+        }
+    };
+    $animationContainer.addEventListener('animationend', removeContainer);
+});
