b27ba53b2cff8b4c85ca263d22cca40924882d31
Jean-Baptiste Bianchi <jb.bianchi@neuroglia.io>
2022-03-11 12:39:09 +0100

    Dashboard - refreshing node when instance collection changes in workflow node model

--- src/dashboard/Synapse.Dashboard/Features/Shared/WorkflowDiagram/WorkflowNodeModel.cs
+++ src/dashboard/Synapse.Dashboard/Features/Shared/WorkflowDiagram/WorkflowNodeModel.cs
@@ -1,6 +1,8 @@
-﻿using Blazor.Diagrams.Core.Models;
+﻿using Blazor.Diagrams.Core.Geometry;
+using Blazor.Diagrams.Core.Models;
 using Synapse.Integration.Models;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 
 namespace Synapse.Dashboard
 {
@@ -18,6 +20,22 @@ namespace Synapse.Dashboard
         /// <inheritdoc/>
         public ObservableCollection<V1WorkflowInstance> FaultedInstances { get; } = new();
 
+        public WorkflowNodeModel(Point? position = null, RenderLayer layer = RenderLayer.HTML, ShapeDefiner? shape = null)
+            : base(position, layer, shape)
+        {
+            this.ActiveInstances.CollectionChanged += this.OnCollectionChanged;
+        }
+
+        public WorkflowNodeModel(string id, Point? position = null, RenderLayer layer = RenderLayer.HTML, ShapeDefiner? shape = null)
+            : base(id, position, layer, shape)
+        {
+            this.ActiveInstances.CollectionChanged += this.OnCollectionChanged;
+        }
+
+        protected void OnCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            this.Refresh();
+        }
     }
 
 }
