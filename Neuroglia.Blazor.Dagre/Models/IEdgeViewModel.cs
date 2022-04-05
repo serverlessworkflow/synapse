cf341ee3b0f315e1d1b02097a8e363839a06132a
Jean-Baptiste Bianchi <jb.bianchi@neuroglia.io>
2022-04-05 17:36:33 +0200

    Dagre - added edge labels rendering PoC

--- Neuroglia.Blazor.Dagre/Models/IEdgeViewModel.cs
+++ Neuroglia.Blazor.Dagre/Models/IEdgeViewModel.cs
@@ -6,8 +6,11 @@
         Guid SourceId { get; set; }
         Guid TargetId { get; set; }
         ICollection<IPosition> Points { get; set; }
+        string? LabelPosition { get; set; }
+        double? LabelOffset { get; set; }
         string Shape { get; set; }
         string? StartMarkerId { get; set; }
         string? EndMarkerId { get; set; }
+        IBoundingBox BBox { get; }
     }
 }
